8a69fa8cd09455fcc83866d9d1ef672d2a62b810
Nuno <e_npgr@yahoo.com>
2015-12-17 14:20:56 -0430

    app.js, main.html, db/app, node_modules/crud5

---
+++ app.js
@@ -0,0 +1,30 @@
+var app = require('app')
+var BrowserWindow = require('browser-window')
+var fs = require('fs')
+const ipcMain = require('electron').ipcMain;
+
+app.on('ready', function() {
+
+//--- Main Window
+	var mainWindow = new BrowserWindow({
+		width: 1020,
+		height: 700,
+		y: 50,
+		//show: false,
+		//frame: false  //frameless window
+	})
+	mainWindow.openDevTools()
+	mainWindow.on('closed', function() {
+		mainWindow = null;
+	});
+	
+	mainWindow.loadURL('file://'+__dirname+'/main.html')
+	//mainWindow.loadURL('http://localhost:1337')
+})
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
