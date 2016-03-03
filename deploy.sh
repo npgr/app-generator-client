6a3862830834648471d7aee43043b77c216c0f24
Nuno <e_npgr@yahoo.com>
2016-03-03 17:51:07 -0430

    deploy.sh + todos +

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#rm -r deploy/node_modules
+
+#cp node_modules deploy/node_modules
+
+#Remove unnecesary files & folders 
+
+find deploy/node_modules -iname 'examples' -exec rm -r {} \;
+
+find deploy/node_modules -iname 'docs' -exec rm -r {} \;
+
+find deploy/node_modules -iname 'test' -exec rm -r {} \;
+
+find deploy/node_modules -iname '*.md' -exec rm {} \;
+
+find deploy/node_modules -iname 'License*' -exec rm {} \;
+
+find deploy/node_modules -iname 'changelog' -exec rm {} \;
+
+find deploy/node_modules -iname 'readme*' -exec rm {} \;
+
+# Compact all .js files
+# node_modules -> it takes about 1 hour
+find deploy/node_modules -iname '*.js' -exec uglifyjs --compress --mangle -o {} -- {} \;
+
+# separar html, css y js en paginas web-> uglifyjs .js y html-minifier .html ¿ cssmin ?
+
+# remove assets/components/ not used
+
+# delete from Controllers the generate options
+
+
+# Vulcanize
+
+# vulcanize --inline-css --inline-scripts reporte.html > reporte2.html
