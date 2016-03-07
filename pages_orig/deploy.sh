7ce17e90d4f2bbd89b94af077bd21e2d4e05e892
Nuno <e_npgr@yahoo.com>
2016-03-07 17:55:06 -0430

    divide pages(minify & obfuscated) from pages for generate pages.asar

---
+++ pages_orig/deploy.sh
@@ -0,0 +1,8 @@
+html-minifier --minify-js --minify-css --remove-comments --output ../pages/newApp.html newApp.html
+
+html-minifier --minify-js --minify-css --remove-comments --output ../pages/createMFunction.html createMFunction.html
+
+html-minifier --minify-js --minify-css --remove-comments --output ../pages/createModel.html createModel.html
+
+html-minifier --minify-js --minify-css --remove-comments --output ../pages/startApp.html startApp.html
+
