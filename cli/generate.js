c5d2d8d5b494223f46e7e30b4708c2e43c720784
Nuno <e_npgr@yahoo.com>
2016-05-17 23:19:00 -0400

    cli - create app :node_modules progress & shelljs + webserver.tar.gz + node_modules.tar.gz

--- cli/generate.js
+++ cli/generate.js
@@ -18,7 +18,82 @@ program
 
 function create_app(app_name)
 {
-	console.log('ready for create app: %s', app_name )
+	console.log('Creating Dir %s', app_name )
+	
+	require('shelljs/global');
+	mkdir(app_name)
+	
+	/** decompress App */
+	echo('Extracting webserver.tar')
+	
+	var ProgressBar = require('progress');
+	var bar = new ProgressBar('[:bar] :percent / :elapseds - ETA :etas', {
+		complete: '=',
+		incomplete: ' ',
+		width: 30,
+		total: 1936
+	});
+	
+	var readline  = require('readline');
+	var child = exec('tar -xvf webserver.tar.gz -C '+app_name, {async:true, silent:true});
+	
+	count = 0
+	readline.createInterface({
+		input     : child.stdout,
+		terminal  : false
+	}).on('line', function(line) {
+		//console.log(line);
+		count++
+		if ((count % 50) == 0) 
+			bar.tick(50);
+		if (count == 1936)
+			bar.tick(36)
+	});
+	
+	child.on('close', function (code, signal) {
+		//echo('\n')
+		extract_node_modules(app_name)
+	})
+}
+
+function extract_node_modules(app_name)
+{
+	//require('shelljs/global');
+	var ProgressBar = require('progress');
+	var readline  = require('readline');
+	
+	/** Decompress node_modules **/
+	echo('Extracting node_modules.tar')
+	
+	var bar2 = new ProgressBar('[:bar] :percent / :elapseds - ETA :etas', {
+		complete: '=',
+		incomplete: ' ',
+		width: 30,
+		total: 13860
+	});
+
+	var child2 = exec('tar -xvf node_modules.tar.gz -C '+app_name, {async:true, silent:true})
+	count = 0
+	readline.createInterface({
+		input     : child2.stdout,
+		terminal  : false
+	}).on('line', function(line) {
+		//console.log(line);
+		count++
+		if ((count % 100) == 0) 
+			bar2.tick(100);
+		if (count == 13860)
+			bar2.tick(60)
+	});
+	
+	/*if (exec('tar -xf node_modules.tar.gz -C '+app_name).code == 0) {
+		echo('Application %s Created', app_name)
+		echo('Finish')
+	} else {
+		echo('Error creating App');
+		return
+	}*/
+	
 }
 
 function generate_crud(model_name)
